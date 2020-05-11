1c4482331271cf12bb7ed6800a7e0ef2903b39ef
Vik Borges <v1k@protonmail.com>
2020-05-11 17:31:19 +0100

    added python version; requirements for dependencies; modified test script to use cpu for inference

--- u2net_test.py
+++ u2net_test.py
@@ -54,7 +54,8 @@ def save_output(image_name,pred,d_dir):
 def main():
 
     # --------- 1. get image path and name ---------
-    model_name='u2net'#u2netp
+    model_name='u2net'
+    #model_name='u2netp'
 
 
     image_dir = './test_data/test_images/'
@@ -83,7 +84,7 @@ def main():
     elif(model_name=='u2netp'):
         print("...load U2NEP---4.7 MB")
         net = U2NETP(3,1)
-    net.load_state_dict(torch.load(model_dir))
+    net.load_state_dict(torch.load(model_dir, map_location='cpu'))
     if torch.cuda.is_available():
         net.cuda()
     net.eval()
