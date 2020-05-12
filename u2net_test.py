a6b45562ed87cc3d3d1f1672722c8dfe45f0cee5
Vik Borges <v1k@protonmail.com>
2020-05-12 11:16:09 +0100

    added colored output mods
    context: https://github.com/NathanUA/U-2-Net/pull/5/files?file-filters%5B%5D=.py

--- u2net_test.py
+++ u2net_test.py
@@ -6,7 +6,8 @@ from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms#, utils
+from torchvision import transforms  # , utils
+import argparse
 # import torch.optim as optim
 
 import numpy as np
@@ -18,57 +19,78 @@ from data_loader import ToTensor
 from data_loader import ToTensorLab
 from data_loader import SalObjDataset
 
-from model import U2NET # full size version 173.6 MB
-from model import U2NETP # small version u2net 4.7 MB
+from model import U2NET  # full size version 173.6 MB
+from model import U2NETP  # small version u2net 4.7 MB
 
 # normalize the predicted SOD probability map
+
+
 def normPRED(d):
     ma = torch.max(d)
     mi = torch.min(d)
 
-    dn = (d-mi)/(ma-mi)
+    dn = (d - mi) / (ma - mi)
 
     return dn
 
-def save_output(image_name,pred,d_dir):
+def save_output(image_name, pred, d_dir, colored=False):
 
     predict = pred
     predict = predict.squeeze()
     predict_np = predict.cpu().data.numpy()
 
-    im = Image.fromarray(predict_np*255).convert('RGB')
+    im = Image.fromarray(predict_np * 255).convert('RGB')
     img_name = image_name.split("/")[-1]
     image = io.imread(image_name)
-    imo = im.resize((image.shape[1],image.shape[0]),resample=Image.BILINEAR)
+    imo = im.resize((image.shape[1], image.shape[0]), resample=Image.BILINEAR)
 
     pb_np = np.array(imo)
 
     aaa = img_name.split(".")
     bbb = aaa[0:-1]
     imidx = bbb[0]
-    for i in range(1,len(bbb)):
+    for i in range(1, len(bbb)):
         imidx = imidx + "." + bbb[i]
 
-    imo.save(d_dir+imidx+'.png')
+    imo.save(d_dir + imidx + '.png')
 
-def main():
+    if colored:
+        oriimg = Image.open(image_name)
+        bin_image = predict_np * 255
+        bin_image = Image.fromarray(bin_image).convert('RGB')
+        bin_image = bin_image.resize((oriimg.width, oriimg.height), resample=Image.BILINEAR)
+        bin_image = np.array(bin_image)
+        bin_image = np.where(bin_image > 200, 1, 0)
+        colored_img = bin_image * np.array(oriimg)
+        colored_img = Image.fromarray(colored_img.astype(np.uint8))
+        img = colored_img.convert("RGBA")
+        datas = img.getdata()
+        newData = []
+        for item in datas:
+            if item[0] == 0 and item[1] == 0 and item[2] == 0:
+                newData.append((0, 0, 0, 0))
+            else:
+                newData.append(item)
+        img.putdata(newData)
+        img.save(d_dir + imidx + '_COLORED.png', 'PNG')
+
+def main(colored=False):
 
     # --------- 1. get image path and name ---------
-    model_name='u2net'
-    #model_name='u2netp'
-
+    model_name = 'u2net'
+    # model_name='u2netp'
 
     image_dir = './test_data/test_images/'
     prediction_dir = './test_data/' + model_name + '_results/'
-    model_dir = './saved_models/'+ model_name + '/' + model_name + '.pth'
+    model_dir = './saved_models/' + model_name + '/' + model_name + '.pth'
 
     img_name_list = glob.glob(image_dir + '*')
     print(img_name_list)
 
     # --------- 2. dataloader ---------
-    #1. dataloader
-    test_salobj_dataset = SalObjDataset(img_name_list = img_name_list,
-                                        lbl_name_list = [],
+    # 1. dataloader
+    test_salobj_dataset = SalObjDataset(img_name_list=img_name_list,
+                                        lbl_name_list=[],
                                         transform=transforms.Compose([RescaleT(320),
                                                                       ToTensorLab(flag=0)])
                                         )
@@ -78,13 +100,13 @@ def main():
                                         num_workers=1)
 
     # --------- 3. model define ---------
-    if(model_name=='u2net'):
+    if(model_name == 'u2net'):
         print("...load U2NET---173.6 MB")
-        net = U2NET(3,1)
-    elif(model_name=='u2netp'):
+        net = U2NET(3, 1)
+    elif(model_name == 'u2netp'):
         print("...load U2NEP---4.7 MB")
-        net = U2NETP(3,1)
-        
+        net = U2NETP(3, 1)
+
     if torch.cuda.is_available():
         net.load_state_dict(torch.load(model_dir))
         net.cuda()
@@ -96,7 +118,7 @@ def main():
     # --------- 4. inference for each image ---------
     for i_test, data_test in enumerate(test_salobj_dataloader):
 
-        print("inferencing:",img_name_list[i_test].split("/")[-1])
+        print("inferencing:", img_name_list[i_test].split("/")[-1])
 
         inputs_test = data_test['image']
         inputs_test = inputs_test.type(torch.FloatTensor)
@@ -106,16 +128,20 @@ def main():
         else:
             inputs_test = Variable(inputs_test)
 
-        d1,d2,d3,d4,d5,d6,d7= net(inputs_test)
+        d1, d2, d3, d4, d5, d6, d7 = net(inputs_test)
 
         # normalization
-        pred = d1[:,0,:,:]
+        pred = d1[:, 0, :, :]
         pred = normPRED(pred)
 
         # save results to test_results folder
-        save_output(img_name_list[i_test],pred,prediction_dir)
+        save_output(img_name_list[i_test], pred, prediction_dir, colored=colored)
 
-        del d1,d2,d3,d4,d5,d6,d7
+        del d1, d2, d3, d4, d5, d6, d7
+
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--colored', default=False, type=bool, dest='colored', help='Save the colored version of the result. Default=False.')
+    args = parser.parse_args()
+    main(args.colored)
