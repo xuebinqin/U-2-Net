80e029d96f90ba4b1e34d3c2a5a134d4c4ee5586
leesangha <37141545+leesangha@users.noreply.github.com>
2020-09-09 15:38:06 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -16,12 +16,104 @@ import numpy as np
 app = Flask(__name__, template_folder="templates", static_url_path="/static")
 
 # Init Cartoonizer and load its weights
-wb_cartoonizer = WB_Cartoonize(os.path.abspath("test_code/saved_models/"))
+
 
 requests_queue = Queue()
 BATCH_SIZE = 1
 CHECK_INTERVAL = 0.1
 ##################################################################
+
+# Image Convert function
+
+
+def convert_bytes_to_image(img_bytes):
+    """Convert bytes to numpy array
+    Args:
+        img_bytes (bytes): Image bytes read from flask.
+    Returns:
+        [numpy array]: Image numpy array
+    """
+
+    pil_image = Image.open(io.BytesIO(img_bytes))
+    if pil_image.mode == "RGBA":
+        image = Image.new("RGB", pil_image.size, (255, 255, 255))
+        image.paste(pil_image, mask=pil_image.split()[3])
+    else:
+        image = pil_image.convert("RGB")
+
+    image = np.array(image)
+
+    return image
+
+
+# run
+
+
+def run(input_file, file_type, f_path):
+    try:
+        if file_type == "image":
+            f_name = str(uuid.uuid4())
+
+            img = input_file.read()
+
+            # Image Convert
+            image = convert_bytes_to_image(img)
+            original_img = cv2.imread(image, cv2.IMREAD_UNCHANGED)
+            # Original Image Save
+            original_img_name = os.path.join(f_path, f_name+".png")
+            cv2.imwrite(original_img_name, original_img)
+            # Run model
+            os.system('python u2net_test.py')
+            os.system('rm ' + original_img_name)
+            # Save Output Image
+            output_img_name = os.path.join(f_path, f_name+".png")
+            # return result_path
+
+            result_path = cartoonized_img_name
+
+            return result_path
+
+    except Exception as e:
+        print(e)
+        return 500
+# Queueing
+
+
+def handle_requests_by_batch():
+    try:
+        while True:
+            requests_batch = []
+
+            while not (
+                len(requests_batch)
+                >= BATCH_SIZE  # or
+                # (len(requests_batch) > 0 #and time.time() - requests_batch[0]['time'] > BATCH_TIMEOUT)
+            ):
+                try:
+                    requests_batch.append(
+                        requests_queue.get(timeout=CHECK_INTERVAL))
+                except Empty:
+                    continue
+
+            batch_outputs = []
+
+            for request in requests_batch:
+                batch_outputs.append(
+                    run(request["input"][0], request["input"]
+                        [1], request["input"][2])
+                )
+
+            for request, output in zip(requests_batch, batch_outputs):
+                request["output"] = output
+
+    except Exception as e:
+        while not requests_queue.empty():
+            requests_queue.get()
+        print(e)
+
+
+# Thread Start
+threading.Thread(target=handle_requests_by_batch).start()
 
 
 @app.route("/")
@@ -31,8 +123,54 @@ def main():
 
 @app.route("/predict")
 def predict():
-    print('req')
+    try:
+        print(requests_queue.qsize())
+
+        if requests_queue.qsize() >= 1:
+            return jsonify({"message": "Too Many Requests"}), 429
+
+        input_file = request.files["source"]
+        file_type = request.form["file_type"]
+
+        if file_type == "image":
+            if input_file.content_type not in ["image/jpeg", "image/jpg", "image/png"]:
+                return jsonify({"message": "Only support jpeg, jpg or png"}), 400
+
+        # mkdir and path setting
+        f_id = str(uuid.uuid4())
+        f_path = os.path.join(DATA_FOLDER, f_id)
+        os.makedirs(f_path, exist_ok=True)
+
+        req = {"input": [input_file, file_type, f_path]}
+
+        requests_queue.put(req)
+
+        # Thread output response
+        while "output" not in req:
+            time.sleep(CHECK_INTERVAL)
+
+        if req["output"] == 500:
+            return jsonify({"error": "Error! Please upload another file"}), 500
+
+        result_path = req["output"]
+
+        result = send_file(result_path)
+
+        shutil.rmtree(f_path)
+
+        return result
+
+    except Exception as e:
+        print(e)
+
+        return jsonify({"message": "Error! Please upload another file"}), 400
+
     return render_template("hi.html")
+
+
+@app.route("/health")
+def health():
+    return "ok"
 
 
 if __name__ == "__main__":
