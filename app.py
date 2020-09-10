f3232522a55808b981b3d942ad2d54d72660810b
leesangha <37141545+leesangha@users.noreply.github.com>
2020-09-10 17:57:51 +0900

    Update app.py

--- app.py
+++ app.py
@@ -19,6 +19,9 @@ sys.path.insert(0, "./test_code/")
 
 ###################################################################
 app = Flask(__name__, template_folder="templates", static_url_path="/static")
+app.config["MAX_CONTENT_LENGTH"] = 35 * 1024 * 1024
+
+DATA_FOLDER = "data"
 
 # Init Cartoonizer and load its weights
 wb_cartoonizer = WB_Cartoonize(os.path.abspath("test_code/saved_models/"))
@@ -68,6 +71,34 @@ def run(input_file, file_type, f_path):
             )
 
             result_path = cartoonized_img_name
+
+            return result_path
+
+        if file_type == "video":
+            f_name = input_file.filename
+
+            video = input_file
+
+            original_video_path = os.path.join(f_path, f_name)
+            video.save(original_video_path)
+
+            # Slice, Resize and Convert Video to 15fps
+            modified_video_path = os.path.join(
+                f_path, f_name.split(".")[0] + "_modified.mp4"
+            )
+            width_resize = 480
+            os.system(
+                "ffmpeg -hide_banner -loglevel warning -ss 0 -i '{}' -t 10 -filter:v scale={}:-2 -r 15 -c:a copy '{}'".format(
+                    os.path.abspath(original_video_path),
+                    width_resize,
+                    os.path.abspath(modified_video_path),
+                )
+            )
+
+            # if local then "output_uri" is a file path
+            output_uri = wb_cartoonizer.process_video(modified_video_path)
+
+            result_path = output_uri
 
             return result_path
 
