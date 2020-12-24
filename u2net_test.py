4ea81c2f21c22e6d28ee0d84932bd6d4dfaaa205
adakoda <adakoda.jamjampan@gmail.com>
2020-12-24 11:13:03 +0900

    Update u2net_test.py

--- u2net_test.py
+++ u2net_test.py
@@ -84,10 +84,7 @@ def main():
     elif(model_name=='u2netp'):
         print("...load U2NEP---4.7 MB")
         net = U2NETP(3,1)
-    if torch.cuda.is_available():
-        net.load_state_dict(torch.load(model_dir))
-    else:
-        net.load_state_dict(torch.load(model_dir, map_location=torch.device('cpu')))
+    net.load_state_dict(torch.load(model_dir))
     if torch.cuda.is_available():
         net.cuda()
     net.eval()
