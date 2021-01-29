5d7e20d5bad583d22123a4829358f25868c2d79f
Xuebin Qin <xuebin@ualberta.ca>
2021-01-29 08:57:25 -0700

    Update u2net.py

--- model/u2net.py
+++ model/u2net.py
@@ -351,7 +351,7 @@ class U2NET(nn.Module):
         self.side5 = nn.Conv2d(512,out_ch,3,padding=1)
         self.side6 = nn.Conv2d(512,out_ch,3,padding=1)
 
-        self.outconv = nn.Conv2d(6,out_ch,1)
+        self.outconv = nn.Conv2d(6*out_ch,out_ch,1)
 
     def forward(self,x):
 
@@ -456,7 +456,7 @@ class U2NETP(nn.Module):
         self.side5 = nn.Conv2d(64,out_ch,3,padding=1)
         self.side6 = nn.Conv2d(64,out_ch,3,padding=1)
 
-        self.outconv = nn.Conv2d(6,out_ch,1)
+        self.outconv = nn.Conv2d(6*out_ch,out_ch,1)
 
     def forward(self,x):
 
