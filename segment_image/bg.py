a19c4fbc39b8854beb2ac7c64f7e4955d5a21fd5
Mahesh Deshwal <50293852+deshwalmahesh@users.noreply.github.com>
2022-07-21 21:18:37 +0530

    Update bg.py

--- segment_image/bg.py
+++ segment_image/bg.py
@@ -1,4 +1,51 @@
 import io
+from typing import List, Union, Callable
+
+import cv2
+import numpy as np
+from PIL import Image
+from PIL.Image import Image as PILImage
+
+from .session_factory import new_session
+
+session = new_session("u2net")
+
+
+def naive_cutout(img: PILImage, mask: PILImage) -> PILImage:
+    empty = Image.new("RGBA", (img.size), 0)
+    cutout = Image.composite(img, empty, mask)
+    return cutout
+
+
+def get_concat_v_multi(imgs: List[PILImage]) -> PILImage:
+    pivot = imgs.pop(0)
+    for im in imgs:
+        pivot = get_concat_v(pivot, im)
+    return pivot
+
+
+def get_concat_v(img1: PILImage, img2: PILImage) -> PILImage:
+    dst = Image.new("RGBA", (img1.width, img1.height + img2.height))
+    dst.paste(img1, (0, 0))
+    dst.paste(img2, (0, img1.height))
+    return dst
+
+
+def post_process_mask(mask:np.ndarray)->np.ndarray:
+    '''
+    Post Process the mask for a smooth boundary by applying Morphological Operations
+    https://www.sciencedirect.com/science/article/pii/S2352914821000757
+    args:
+        mask: Binary Numpy Mask
+    '''
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
+    mask = cv2.morphologyEx(mask,cv2.MORPH_OPEN,kernel)
+    mask = cv2.GaussianBlur(mask, (5,5), sigmaX = 1.5, sigmaY = 1.5, borderType = cv2.BORDER_DEFAULT) # Blur
+    mask = np.where( mask < 127, 0, 255).astype(np.uint8) # convert again to binary
+    return mask
+
+
+import io
 from typing import List, Union
 
 import cv2
@@ -45,19 +92,18 @@ def post_process_mask(mask:np.ndarray)->np.ndarray:
     return mask
 
 
-def remove(
-    data: Union[bytes, PILImage, np.ndarray],
-    post_process:bool = True
-) -> Union[bytes, PILImage, np.ndarray]:
-
-    img = data
+def generate_mask(img: Union[bytes, PILImage, np.ndarray],
+    post_process:Callable = None,
+    return_mask_only:bool = False) -> Union[bytes, PILImage, np.ndarray]:
 
     masks = session.predict(img)
     cutouts = []
 
     for mask in masks:
-        if post_process:
+        if post_process is not None:
             mask = Image.fromarray(post_process_mask(np.array(mask))) # Apply post processing to mask
+        
+        if return_mask_only: return mask
         
         cutout = naive_cutout(img, mask)
 
@@ -68,8 +114,4 @@ def remove(
     else:
         cutout = img
 
-    bio = io.BytesIO()
-    cutout.save(bio, "PNG")
-    bio.seek(0)
-
-    return bio.read()
+    return cutout
